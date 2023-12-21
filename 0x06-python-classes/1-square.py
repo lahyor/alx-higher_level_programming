2956f0f6d91629b67a62bccc0b586a72fa1eff14
Otekaye Seun <otekayeseun24@gmail.com>
2023-12-20 17:22:20 -0800

    "O ti remi"

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""A class Square"""
+
+
+class Square:
+    """A Square"""
+
+    def __init__(self, size):
+        """A new Square
+
+        Args:
+            size(int): size of the new square
+        """
+        self.__size = size
